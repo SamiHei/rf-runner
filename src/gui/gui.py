4fa12b7dd6814477a2267577f38cbe54fa273681
Sami Heikkinen <zami.heikkinen@hotmail.com>
2022-11-02 19:45:23 +0200

    GUI element positioning, check if any cases are selected

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -15,8 +15,8 @@ class Gui(tk.Tk):
         self.tr = TestReader()
         self.selected_file = None
         self.application_title = "RF Runner"
-        self.button_padding = "10 10 10 10"
-        self.window_size = "400x700"
+        self.button_padding = "5 5 5 5"
+        self.window_size = "600x800"
 
 
     def start_application(self):
@@ -25,22 +25,39 @@ class Gui(tk.Tk):
 
     def create_gui(self):
         self.geometry(self.window_size)
+        self.minsize(600, 800)
+        self.maxsize(600, 800)
         self.title(self.application_title)
 
-        list_box = tk.Listbox(self, selectmode="multiple")
-        list_box.grid(row=1)
+        first_frame = tk.Frame(self)
+        first_frame.grid(row=0, column=0)
 
-        button1 = ttk.Button(self, text="File", padding=self.button_padding, command=lambda : self.write_tests_to_lb(list_box)).grid(column=0, row=0)
+        second_frame = tk.Frame(self)
+        second_frame.grid(row=1, column=0)
 
-        button2 = ttk.Button(self, text="Quit", padding=self.button_padding, command=self.destroy).grid(column=0, row=2)
-        button3 = ttk.Button(self, text="Run", padding=self.button_padding, command=lambda : self.run_tests(list_box, self.selected_file, target_folder_text.get(), rand.get())).grid(column=1, row=2)
+        third_frame = tk.Frame(self)
+        third_frame.grid(row=2, column=0)
 
-        target_folder_label = tk.Label(self, text="Report target folder").grid(column=0, row=3)
+        fourth_frame = tk.Frame(self)
+        fourth_frame.grid(row=3, column=0)
+
+        list_box = tk.Listbox(first_frame, width=65, height=30, selectmode="multiple")
+        list_box.grid(column=0, row=0, padx=35, pady=5)
+
+        ttk.Button(second_frame, text="File", padding=self.button_padding, command=lambda : self.write_tests_to_lb(list_box)).grid(column=0, row=0, padx=5)
+        ttk.Button(second_frame, text="Select All", padding=self.button_padding, command=lambda : list_box.select_set(0, tk.END)).grid(column=1, row=0, padx=5)
+        ttk.Button(second_frame, text="Clear Selection", padding=self.button_padding, command=lambda : list_box.selection_clear(0, tk.END)).grid(column=2, row=0, padx=5)
+
+        tk.Label(third_frame, text="Report target folder").grid(column=0, row=0, padx=(0, 27.5), pady=(15, 0))
         target_folder_text = tk.StringVar()
-        target_folder_entry = ttk.Entry(self, textvariable=target_folder_text).grid(column=0, row=4)
+        target_folder_entry = ttk.Entry(third_frame, textvariable=target_folder_text).grid(column=0, row=1, padx=25)
 
         rand = tk.IntVar()
-        randomize = tk.Checkbutton(self, text="Randomize test order", variable=rand).grid(column=0, row=5)
+        tk.Checkbutton(third_frame, text="Randomize test order", variable=rand).grid(column=0, row=2, padx=(0, 15))
+        ttk.Button(third_frame, text="Run", padding=self.button_padding, command=lambda : self.run_tests(list_box, self.selected_file, target_folder_text.get(), rand.get())).grid(column=1, row=1, padx=25)
+
+        ttk.Button(fourth_frame, text="Quit", padding=self.button_padding, command=self.destroy).grid(column=0, row=0, padx=(215,0), pady=(50,0))
+
 
 
     def write_tests_to_lb(self, list_box):
@@ -72,10 +89,13 @@ class Gui(tk.Tk):
             for s in selections:
                 cases.append(list_box.get(s))
 
-            command = self.tr.create_robot_command(cases, test_file, target_folder, rand)
+            if (cases == []):
+                print("Select cases to run")
+            else:
+                command = self.tr.create_robot_command(cases, test_file, target_folder, rand)
 
-            print(command)
-            os.system(command)
+                print(command)
+                os.system(command)
         except TypeError:
             print("File not selected!")
             time.sleep(2)
